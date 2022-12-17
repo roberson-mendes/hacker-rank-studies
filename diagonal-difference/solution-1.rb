0e29354d420dd95f835a7635a7ff7affa07b7a29
roberson-mendes <roberson.mendes.c@gmail.com>
2022-12-17 17:04:39 -0300

    feat: first solution from diagonal-difference problem
    - solve by brutal force

---
+++ diagonal-difference/solution-1.rb
@@ -0,0 +1,23 @@
+def diagonalDifference(arr)
+  #go through left diagonal summing its numbers and save into left_diagonal_sum
+  initial_row = 0
+  final_row = arr[0].size - 1
+  left_diagonal_sum = 0
+
+  for i in initial_row..final_row do
+      diagonal_column_index, diagonal_row_index = i, i
+      left_diagonal_sum += arr[diagonal_row_index][diagonal_column_index]
+  end
+
+  #go through right diagonal summing its numbers and save into right_diagonal_sum
+  right_diagonal_sum = 0
+  right_diagonal_column = arr[0].size - 1
+
+  for i in initial_row..final_row do
+      diagonal_row_index = i
+      right_diagonal_sum += arr[diagonal_row_index][right_diagonal_column]
+      right_diagonal_column -= 1
+  end
+
+  (left_diagonal_sum - right_diagonal_sum).abs
+end
