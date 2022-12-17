993db9336316b259a6318dcae0f1d06315584609
roberson-mendes <roberson.mendes.c@gmail.com>
2022-12-17 16:52:48 -0300

    style: refactor am-pm exercise

--- am-pm/am-pm.rb
+++ am-pm/am-pm.rb
@@ -1,60 +1,87 @@
 class HourMap
-    def to_24_format(hour12Format)
-        midDay = "12"
-        midNight24Format = "00"
+    def self.to_24_format(hour_12_format)
+        mid_night_24_format = "00"
+        mid_day_24_format = "12"
+        hour = hour_12_format[:hour]
 
-        return midNight24Format if midNight?(hour12Format)
-        return hour12Format[:hour] if beforeMidDay?(hour12Format[:am_pm_symbol]) || midDay?(hour12Format)
-        return (midDay.to_i + hour12Format[:hour].to_i)
+        case hour_12_format
+            when mid_night
+                mid_night_24_format
+            when mid_day
+                mid_day_24_format
+            when before_mid_day(hour_12_format)
+                hour
+            else
+                to_24 = -> (hour_12_format) { mid_day.to_i + hour_12_format.to_i }
+                to_24.call(hour_12_format[:hour])
+        end
     end
 
     private
 
-    def midNight?(hour12Format)
-        midNight12Format = {
-            hour: "12",
-            am_pm_symbol: "AM"
-        }
-
-        hour12Format == midNight12Format
-    end
-
-    def beforeMidDay?(am_pm_symbol)
-        beforeMidDay = "AM"
-        am_pm_symbol == beforeMidDay
-    end
-
-    def midDay?(hour12Format)"00:40:22"
-        midDay12Format = {
+    def self.mid_day
+        mid_day_12_format = {
             hour: "12",
             am_pm_symbol: "PM"
         }
+    end
 
-        hour12Format == midDay12Format
+    def self.mid_night
+        mid_night_12_format = {
+            hour: "12",
+            am_pm_symbol: "AM"
+        }
+    end
+
+    def self.before_mid_day(hour_12_format)
+        proc { |hour_12_format| hour_12_format[:am_pm_symbol] == "AM"}
     end
 end
 
 def slice_hour_representation(str)
-    init_am_pm = 8
-    end_am_pm = 9
+    init_am_pm_string = 8
+    end_am_pm_string = 9
     {
-        am_pm_symbol: str.slice!(init_am_pm..end_am_pm),
+        am_pm_symbol: str.slice!(init_am_pm_string..end_am_pm_string),
         hour: str.slice!(0..1)
     }
 end
 
 def timeConversion(s)
-    hour12Format = slice_hour_representation(s)
+    hour_12_format = slice_hour_representation(s)
 
-    hour24Format = HourMap.new().to_24_format(hour12Format)
+    hour24Format = HourMap.to_24_format(hour_12_format)
 
     s.insert(0, hour24Format.to_s)
 end
 
-def test
+def test_mid_night
     time_to_convert = "12:40:22AM"
     expected_result = "00:40:22"
-    puts (timeConversion(time_to_convert) == expected_result)
+
+    function_result = timeConversion(time_to_convert)
+
+    puts ("passed: #{function_result == expected_result}")
 end
 
-test
+def test_mid_day
+    time_to_convert = "12:40:22PM"
+    expected_result = "12:40:22"
+
+    function_result = timeConversion(time_to_convert)
+
+    puts ("passed: #{function_result == expected_result}")
+end
+
+def test_before_mid_day
+    time_to_convert = "08:40:22AM"
+    expected_result = "08:40:22"
+
+    function_result = timeConversion(time_to_convert)
+
+    puts ("passed: #{function_result == expected_result}")
+end
+
+test_mid_night
+test_mid_day
+test_before_mid_day
