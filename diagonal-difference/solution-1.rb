a04c5820e7524cda4d4469270c62dba6a57c1669
roberson-mendes <roberson.mendes.c@gmail.com>
2022-12-17 17:23:47 -0300

    refac: refactores diagonal-difference solution-1
    - Separates codes into methods

--- diagonal-difference/solution-1.rb
+++ diagonal-difference/solution-1.rb
@@ -21,3 +21,36 @@ def diagonalDifference(arr)
 
   (left_diagonal_sum - right_diagonal_sum).abs
 end
+
+######################################################################---REFACTOR 1
+def left_diagonal_sum(arr)
+  initial_row = 0
+  final_row = arr[0].size - 1
+  left_diagonal_sum = 0
+
+  for i in initial_row..final_row do
+      diagonal_column_index, diagonal_row_index = i, i
+      left_diagonal_sum += arr[diagonal_row_index][diagonal_column_index]
+  end
+
+  left_diagonal_sum
+end
+
+def right_diagonal_sum(arr)
+  initial_row = 0
+  final_row = arr[0].size - 1
+  right_diagonal_sum = 0
+  right_diagonal_column = arr[0].size - 1
+
+  for i in initial_row..final_row do
+      diagonal_row_index = i
+      right_diagonal_sum += arr[diagonal_row_index][right_diagonal_column]
+      right_diagonal_column -= 1
+  end
+
+  right_diagonal_sum
+end
+
+def diagonalDifference(arr)
+  (left_diagonal_sum(arr) - right_diagonal_sum(arr)).abs
+end
