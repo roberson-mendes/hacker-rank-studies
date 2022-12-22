cee58c53903668b0a7a3e9328696b7be51444a14
roberson-mendes <roberson.mendes.c@gmail.com>
2022-12-22 16:27:28 -0300

    feat: adds solution 2 into permuting-two-arrays problem

--- permuting-two-arrays/permuting-two-arrays.rb
+++ permuting-two-arrays/permuting-two-arrays.rb
@@ -52,6 +52,31 @@ def twoArrays(k, a, b)
   Permutation.new.possible_to_permute?(a, b, k) ? "YES" : "NO"
 end
 
+#######################################################################---SOLUTION 2
+=begin
+Smarter solution: instead of search the minimum b to each a, we do almost the same
+#but by sorting the arrays, a in descending order, b in ascending order, we have each of
+small value from a added to each bigger value from b, then we have that for each number
+of a, we have a possible sum on b such as a+b is >= k. If some of the numbers of a doesn't
+have any b in which the sum >= k, so it's not possible to sort the arrays in any order that
+satisfies the condition a'[i] + b'[i] >= k
+=end
+
+def twoArrays2(k, a, b)
+  a.sort!.reverse!
+  b.sort!
+  condition_matches_for_all = Proc.new do
+    a.each_with_index do |a_value, index|
+      return "NO" if a_value + b[index] < k
+    end
+    return "YES"
+  end
+
+  condition_matches_for_all.call()
+end
+
+
+#######################################################################---TESTS SOLUTION 1
 def test1
   expected_result = "YES"
   k = 4
@@ -96,7 +121,55 @@ def test4
   p "passed: #{result == expected_result}"
 end
 
+def test_solution2_1
+  expected_result = "YES"
+  k = 4
+  a = [1, 3]
+  b = [3, 1]
+
+  result = twoArrays2(k, a, b)
+
+  p "passed: #{result == expected_result}"
+end
+
+def test_solution2_2
+  expected_result = "NO"
+  k = 5
+  a = [2, 3, 1, 1, 1]
+  b = [1, 3, 4, 3, 3]
+
+  result = twoArrays2(k, a, b)
+
+  p "passed: #{result == expected_result}"
+end
+
+def test_solution2_3
+  expected_result = "NO"
+  k = 9
+  a = [1, 5, 1, 4, 4, 2, 7, 1, 2, 2]
+  b = [8, 7, 1, 7, 7, 4, 4, 3, 6, 7]
+
+  result = twoArrays2(k, a, b)
+
+  p "passed: #{result == expected_result}"
+end
+
+def test_solution2_4
+  expected_result = "YES"
+  k = 9
+  a = [3, 6, 8, 5, 9, 9, 4, 8, 4, 7]
+  b = [5, 1, 0, 1, 6, 4, 1, 7, 4, 3]
+
+  result = twoArrays2(k, a, b)
+
+  p "passed: #{result == expected_result}"
+end
+
 test1
 test2
 test3
 test4
+test_solution2_1
+test_solution2_2
+test_solution2_3
+test_solution2_4
